b1226974fdad0494518b2765a649c7ec381b3d50
son <son@lincolnix.net>
2014-07-22 02:52:59 +0000

    initial inpsiration on a whim

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 PatrickBradley::Application.routes.draw do
+
+  root "static_pages#home"
+
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'static_pages/gallery'
+
+  get 'static_pages/contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
